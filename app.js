650bd8f655b8d4b37ff6f543d37eb9c147715531
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-04 14:08:42 +0530

    Level-2 Mongoose-encryption

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
@@ -13,10 +15,15 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+//only encrypt certain fields
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,6 +45,7 @@ app.post("/register", function(req,res){
     password: req.body.password
   });
 
+//During save, documents are encrypted and then signed.
   newUser.save(function(err){
     if(err){
       console.log(err);
@@ -51,6 +59,7 @@ app.post("/login", function(req,res){
   const username = req.body.username;
   const password = req.body.password;
 
+//During find, documents are authenticated and then decrypted
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
