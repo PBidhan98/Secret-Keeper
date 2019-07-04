3ee0639116f9803bc52d50899db3479722a1a018
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-04 14:47:13 +0530

    Hashing the Password

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,10 +19,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-//only encrypt certain fields
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +37,7 @@ app.get("/register",function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
 //During save, documents are encrypted and then signed.
@@ -56,7 +52,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
 //During find, documents are authenticated and then decrypted
   User.findOne({email: username}, function(err, foundUser){
@@ -71,9 +67,6 @@ app.post("/login", function(req,res){
     }
   });
 });
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
